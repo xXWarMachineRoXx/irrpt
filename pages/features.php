e12027d07008dce35da5de5441af7a782540a457
Rijul_Sharma <rijul.sharma@tech-bridge.biz>
2023-01-30 10:52:38 +0530

    bhumij edit

--- pages/features.php
+++ pages/features.php
@@ -6,16 +6,25 @@ include "../php/components/header.php";
 
 ?>
 
-<body class="d-flex h-100 text-center text-white bg-dark">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+<body data-bs-theme="light">
+    <div class="d-flex  text-center   ">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
-        <header class="mb-auto">
-            <div>
-                <h3 class="float-md-start mb-0">tbRC</h3>
-                <nav class="nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link " aria-current="page" href="/irrpt">Home</a>
-                    <a class="nav-link active" href="#">Features</a>
-                    <a class="nav-link" href="#">Contact</a>
-                </nav>
+            <?php echo pHeader(); ?>
+        </div>
+    </div>
+    <div class="d-flex mt-5 text-center  ">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+            <div class="container" >
+                <div class="card">
+                    <h5 class="card-header">Featured</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-dark">Go somewhere</a>
+                    </div>
+                </div>
             </div>
-        </header>
+        </div>
+    </div>
--- php/components/header.php
+++ php/components/header.php
@@ -13,3 +13,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
+
+<?php
+
+function pHeader(){
+
+    $sHtml = '<header class="mb-auto">
+            <div>
+                <h3 class="float-md-start mb-0">tbRC</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link " aria-current="page" href="/irrpt">Home</a>
+                    <a class="nav-link active" href="#">Features</a>
+                    <a class="nav-link" href="#">Contact</a>
+                </nav>
+            </div>
+        </header>';
+    return $sHtml;
+}
